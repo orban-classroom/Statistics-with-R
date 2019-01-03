7baecacdeb734f427d94389ce49164e92b73a7fe
Levente Orbán <llorban@users.noreply.github.com>
2019-01-03 12:51:32 -0800

    Add files via upload

---
+++ 2019 Spring/lectures/Lecture 1 as delivered.R
@@ -0,0 +1,59 @@
+2+2
+
+3    + 2
+
+6-6
+1*1
+2/2
+5^5
+
+10 = 20
+10 == 20
+10 == 10
+5 + 5 == 10
+
+citation()
+
+citation ()
+citation( )
+
+cita tion()
+
+citation(
+  
+  )
+
+citation(
+  
+)
+
+10 -
+  
+2 * 2 * 2
+2^3
+2^0.5
+sqrt(2)
+
+myvariable <- 200
+myvariable
+150 -> myothervariable
+myothervariable
+
+myOtherVariable
+my_other_variable
+my.other.variable
+
+myothervariable <- 5
+
+myothervariable * myvariable
+200 * 5
+myvariable + 200
+
+cupsOfCoffee <- c(3,2,0,1)
+cupsOfCoffee
+
+cupsOfCoffee + 5
+cupsOfCoffee^2
+
+is.integer(cupsOfCoffee)
+is.numeric(cupsOfCoffee)
