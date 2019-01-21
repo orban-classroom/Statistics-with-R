8973581e92316dfea87fd48315ab8e723881c260
Levente Orbán <llorban@users.noreply.github.com>
2019-01-20 16:42:17 -0800

    Update and rename 3-homework.md to 3-homework.r

---
+++ 2019 Spring/homework/3-homework.r
@@ -0,0 +1,12 @@
+# R Homework 3
+## Deadline January 25th, Friday (exception for delayed homework posting) at 23:55 pst on Moodle
+
+# Copy the data frame we generated last class (containing hrs of sleep, screentime etc). 
+
+# Compute the mean of each of the variables using the "old-fashioned way," using the formula
+
+# Compute the median and mode for each of the variables using the methods we discussed during the lecture
+
+# Generate a histogram for each of the variables, and describe their shape using concept from Chapter 5 (skewness, kurtosis, normality). 
+
+# Save your work as an R script file, and upload it to Moodle. 
