ffe3589bfe2914a9891684e91fcb6ea3c3ef3daa
Levente Orbán <llorban@users.noreply.github.com>
2019-02-08 22:12:16 -0800

    Update 5-homework.r

--- 2019 Spring/homework/5-homework.r
+++ 2019 Spring/homework/5-homework.r
@@ -12,6 +12,9 @@
 ## Vector subsetting
 ##
 
+## Load this vector (hours of sleep) we generated some weeks ago
+var1 <- c(45,49,46,48,49,42,50,42,20,50,49,56,49,40,56,42,45,49,50,56,46,56,35,38,56,56,53,40,42,50)
+
 # Select the fifth element in the list
 
 # Select the 2nd through the 10th element on the list
@@ -21,4 +24,17 @@
 ## Data frame subsetting is complex because there are multiple columns of values
 ##
 
-# 
+## Your class data has been posted as a csv file. Load this URL into a data frame: 
+## https://raw.githubusercontent.com/llorban/KPU-Statistics-2300/master/2019%20Spring/data/2300classdata/data.csv
+
+# Select all those students who do not drink any caffeine. Save this selected data as a new data frame.
+
+# Create a separate data frame for those who drink any amount of caffeine.
+
+# Compute the mean of hours worked for those who drink caffein vs those who do not.
+
+# Select all those students who have more than 50 hours of weekly screen time. 
+
+# Select all those students' records who have less than 50 hours of weekly screen time. 
+
+# Repeat the mean calculations of hours worked for each of these two groups. 
