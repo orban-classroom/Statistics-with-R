8e43e71f81cd14c133a3556c5a082998336b3501
Levente Orbán <llorban@users.noreply.github.com>
2019-02-08 20:54:12 -0800

    Create 5-homework.r
    homework 5 in progress

---
+++ 2019 Spring/homework/5-homework.r
@@ -0,0 +1,24 @@
+##
+## Homework 5
+##
+## Practicing subsetting. 
+## Insert your answer below each question. 
+
+
+## Subsetting vectors and data frames has a similar structure. Use a square bracket [ ] with a data frame 
+## or a vector variable to select or filter the data according various rules
+
+##
+## Vector subsetting
+##
+
+# Select the fifth element in the list
+
+# Select the 2nd through the 10th element on the list
+
+##
+## Data frame subsetting
+## Data frame subsetting is complex because there are multiple columns of values
+##
+
+# 
