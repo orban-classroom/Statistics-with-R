240e74eb01371d0122dc7b3c529daf9f14b8256a
Dr. Levente L. Orbán <llo@kpu.ca>
2019-02-14 09:50:19 -0800

    Lecture 6 as planned

---
+++ 2019 Spring/lectures/Lecture 6 as planned.R
@@ -0,0 +1,50 @@
+
+
+# load an external R file
+source(myfile.r)
+
+## Loading packages (once they are installed)
+library(nameofpackage)
+
+## LOOPS
+
+## for loop
+# for ( VAR in VECTOR ) {
+#  STATEMENT1
+#  STATEMENT2
+#  ETC }
+
+## while loop
+# while ( CONDITION ) {
+#  STATEMENT1
+#  STATEMENT2
+#  ETC }
+
+## IMPLICIT LOOPS
+sapply()
+
+tapply()
+
+## CONDITIONAL
+
+## if function
+# if ( CONDITION ) {
+#   STATEMENT1
+#   STATEMENT2
+#   ETC
+# } else {
+#   STATEMENT3
+#   STATEMENT4
+#   ETC
+# }
+
+## FUNCTIONS
+
+## custom functions
+# FNAME <- function ( ARG1, ARG2, ETC ) {
+#  STATEMENT1
+#  STATEMENT2
+#  ETC
+#  return( VALUE )
+# }
+
