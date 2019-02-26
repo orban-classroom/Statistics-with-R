0c1e471dae086571c13b821220854f6d3fb6e693
Levente Orbán <llorban@users.noreply.github.com>
2019-02-25 18:08:57 -0800

    Create 6-homework.r

---
+++ 2019 Spring/homework/6-homework.r
@@ -0,0 +1,17 @@
+# Homework 6
+
+# Last lecture we loaded a real-world data set from the Surrey city website. In this homework, you will continue to work with these data sets. 
+
+# 1. Load the inspection reports data set (http://data.surrey.ca/dataset/948e994d-74f5-41a2-b3cb-33fa6a98aa96/resource/30b38b66-649f-4507-a632-d5f6f5fe87f1/download/fraserhealthrestaurantinspectionreports.csv)
+
+# 2. Create two filtered variables, one that contains only "Routine" inspections, and another that contains only "Follow-Up" inspections. 
+
+# 3. How many routine, and how many follow-up inspections does the data set contain?
+
+# 4. Which are the top 5 restaurants (identified by tracking number) that receive the most routine visits?
+
+# 5. Which are the top 5 restaurants (identified by tracking number) that receive the most follow-up visits?
+
+# 6. Create a custom function that calculates variance of a variable.
+
+# 7. Using your new custom function, compute the variance of the NumCritical and NumNonCritical values, separately for routine and follow-up visits. 
