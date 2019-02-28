ac3882e10419e6fcdd22d868aebece459a803d31
Levente Orbán <llorban@users.noreply.github.com>
2019-02-28 12:53:14 -0800

    Add files via upload

---
+++ 2019 Spring/lectures/Lecture 7 as delivered.R
@@ -0,0 +1,44 @@
+population1 <- c("black","black","white","white","white","white","black","black","white","black")
+population2 <- c("black","black","black","black","black","black","black","black","black","white")
+
+## number of outcomes classified as black
+
+allblackballs1 <- population1[population1 == "black"]
+allblackballs2 <- population2[population2 == "black"]
+
+blackbag1 <- length(allblackballs1)
+blackbag2 <- length(allblackballs2)
+
+blackbag1
+blackbag2
+
+## the total number of balls in each bag
+
+totalbag1 <- length(population1)
+totalbag2 <- length(population2)
+
+## probability of a black ball
+
+prob.black.1 <- blackbag1/totalbag1
+prob.black.2 <- blackbag2/totalbag2
+
+prob.black.1
+prob.black.2
+
+
+
+## simple vector problem
+
+x <- c(1,1,2,3,3,4,4,4,5,6)
+
+vals.greater.than.four <- length(x[x>4])
+total.vals <- length(x)
+
+prob.greater.than.four <- vals.greater.than.four / total.vals
+prob.greater.than.four
+
+
+## binomial distribution
+
+dbinom(x = 10, size = 100, prob = .5)
+
