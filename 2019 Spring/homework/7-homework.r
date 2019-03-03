9f8ef266b10c03013a9f0c04334e86d09db39785
Levente Orbán <llorban@users.noreply.github.com>
2019-03-03 12:24:02 -0800

    Create 7-homework.r

---
+++ 2019 Spring/homework/7-homework.r
@@ -0,0 +1,16 @@
+## Homework 7
+## Due, Sunday, March 10th
+
+## 1. This exercise involves your kitchen drawer containing cutlery. Count the number of forks, knives, spoons and anything else that you might find in the drawer. Create a vector with character values of the contents of your cutlery drawer. Compute the probabilities of each of the items that could be picked from the drawer (similarly to the black-and-white ball exercise during class). Save part computations as variables and keep your work organized and concise
+
+
+## 2. You will record naturalistic observation data for this part of the homework. Situate yourself somewhere on campus where you can observe people entering through a door over a 10 minute interval (e.g., psychology lab, a lounge area, library). Use a timer (e.g., on your smartphone). Record the number of people who pass each 1-minute interval. Create a vector of the values you recorded. 
+
+## Location of recording
+
+## Vector
+
+## What is the probability that you see 10 or more people pass through your observed area?
+
+## What is the probability that you see no-one (0) pass through your observed area?
+
