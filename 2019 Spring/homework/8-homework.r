afb8605b43b5a33d36322b2ba70602acbba68da3
Dr. Levente L. Orbán <llo@kpu.ca>
2019-03-10 16:39:23 -0700

    homework 8 posted

---
+++ 2019 Spring/homework/8-homework.r
@@ -0,0 +1,69 @@
+# Part 1
+# What's wrong with this code? Type the correct syntax below
+
+# 1
+var1 -> c(1,5,7,3,6,,7,3,2,,6,3)
+
+# 2
+var2 -> c(65,23,54,23,6,67,43,23,54,23))
+
+# 3
+var3 -> c(9,9,9,9,9,7,7,7,"7")
+
+# 4
+length <- (var1)
+mean.hrs.sleep.weekly <- sum(var1)/n
+
+# 5
+n2 <- length(var2)
+n2 <- length(var3)
+mean.cups.caffeine.weekly <- sum(var2)/n2
+
+# 6
+n3 <- length(var3) - 1
+mean.hrs.exercise.weekly <- sum(var3)/n3
+mean.hrs.exercise.weekly
+
+# 7
+hist(var1)
+
+# 8
+hist(var2,
+     main = "Histogram of Weekly Sleep Time")
+
+# 9
+hist(var3, break = 1)
+
+# 10
+var.mean = mean(var)
+centered <- data$var - var.mean
+sqd <- centered
+ss <- sum(sqd)^2
+df <- n-1
+real.msqd <-ss/(n) 
+
+# Part 2
+# A new data file has been added to GitHub: https://raw.githubusercontent.com/llorban/KPU-Statistics-2300/master/2019%20Spring/data/1100examdata/1100_exam_data.csv
+# The data contains two columns of exam scores from two online 1100 courses from 2018. The first column is exam 1, and the second column is exam 2. The three digit values in the column names indicate the total achievable score on the exam. 
+
+# In order to simulate an exam setting, I suggest trying to do this homework without consulting any aids.
+
+# 1. Load the data into a data frame
+
+# 2. Display the column names 
+
+# 3. Visualize each column of data in a histogram. Display the histograms as side-by-side panels
+
+# 4. What can you conclude about the shape of the histograms?
+
+# 5. Center both variables
+
+# 6. Double-check that you correctly centred
+
+# 7. Convert the raw scores to z-scores (i.e., standardize the data)
+
+# 8. What is the probability that the two exam z-scores in the third row are observed?
+
+# 9. Compute the standard error of the mean for each of the variables
+
+# 10. Draw the histograms again, but this time, also draw the SEM on the histograms
