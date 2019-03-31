af7f0b3c07575471e3e809bf6d0928b84ad2ec6a
Levente Orbán <llorban@users.noreply.github.com>
2019-03-31 09:42:27 -0700

    Create 10-homework.r

---
+++ 2019 Spring/homework/10-homework.r
@@ -0,0 +1,5 @@
+
+# Load the chico.csv data set from the GitHub repository under data/textbook_chico
+# The data is described in the textbook on page 401.
+# Using the technique we considered in class, conduct a repeated-measures t-test
+# Use APA guidelines to summarize the results in a Word document. 
