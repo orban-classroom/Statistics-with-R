373aa99a047b9f3f3a664892eb3be1c6704d2ecb
Dr. Levente L. Orbán <llo@kpu.ca>
2019-04-11 08:33:22 -0700

    posting exam 3

---
+++ 2019 Spring/exams/exam3/Exam 3.R
@@ -0,0 +1,30 @@
+# EXAM 3: 90 poinnts
+rm(list = ls())
+
+### Rules
+### 
+### CLOSED BOOK. This is a closed book exam. Only the cheat sheet and R Studio help may be consulted.
+### BE CONCISE. There are penalties to overly verbose code. This means that you save relevant part 
+###     calculations to variables and reuse them wherever needed, throughout the exam. 
+### BE ORGANIZED. *Comment* your code, create headings and use spacing smartly to make your code readable.
+### 
+###
+
+# Data Set: http://bit.ly/2KueIjj
+
+# Research Question: Is there a significant difference 
+# between males and females on their level of education in this sample?
+
+## 1. Check assumptions. Use your knowledge from lectures and the textbook,
+## Also, state your conclusion as comments about whether or not assumptions have been met, and why, 15 points
+
+## 2. Run descriptive statistical analyses, 25 points
+## Justify your choices as comments above each computation
+
+## 2. Conduct the appropriate t-test analysis (even if assumptions are violated), 25 points
+## Justify your choices as comments above each computation
+
+## 3. Visualize the results with a bar plot and your choice of error bars, 15 points
+## Justify your choices as comments above each computation
+
+## 4. Interpret the results using everyday language, 10 points
