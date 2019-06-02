7e221fbfad2236f0246a49951142e215408918b7
Dr. Levente L. Orbán <llo@kpu.ca>
2019-06-01 17:20:56 -0700

    homework 4

---
+++ 2019 Summer/R Lab Homework/4-lab homework.r
@@ -0,0 +1,60 @@
+## R Lab Homework 3
+
+## Follow these stylistic conventions
+## 1. Have your own system of variable naming, and stick to it. I will be looking for consistency.
+## 2. Use a single hashtag # for your comments. I will use ## for my comments.
+## Use spaces carefully to organize your code. Make it readable. You can use hashtags to add headings and comments to your code.  
+
+
+## Deadline May 30th, 2019, 11:55 pm
+
+garbage.data <- read.csv(url("https://raw.githubusercontent.com/llorban/KPU-Statistics-2300/master/2019%20Spring/data/ubcgarbage/garbage_disposal.csv"))
+
+## All your work will be based on the time_at_bin variable.
+
+## Generate a histogram, and describe the shape using concepts from Chapter 5 (skewness, kurtosis, normality). 
+## Add your comments as hashtags
+
+
+## Generate a boxplot. Explain your observations of normality based on quantile measures.
+## Add your comments as hashtags
+
+
+## Compute the mean, the slow way
+
+
+## Double-check your work:
+
+
+## Centre the data (compute deviation scores)
+
+
+## Double-check your work:
+
+
+## Square each deviation score
+
+
+## Double-check your work:
+
+
+## Sum of Squares: Sum the squared deviation scores
+
+
+## Double-check your work:
+
+
+## Compute the variance
+
+
+## Double-check your work:
+
+
+## Compute the standard deviation
+
+
+## Double-check your work:
+
+
+## Save your work as an R script file, and submit it via Moodle. 
+
