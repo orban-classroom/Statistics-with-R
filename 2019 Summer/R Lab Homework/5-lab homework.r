55b38d258b20a3d0e1cf6581998b50654132d290
Dr. Levente L. Orbán <llo@kpu.ca>
2019-06-09 13:07:26 -0700

    homework 5 push

---
+++ 2019 Summer/R Lab Homework/5-lab homework.r
@@ -0,0 +1,74 @@
+## R Lab Homework 3
+
+## Follow these stylistic conventions
+## 1. Have your own system of variable naming, and stick to it. I will be looking for consistency.
+## 2. Use a single hashtag # for your comments. I will use ## for my comments.
+## Use spaces carefully to organize your code. Make it readable. You can use hashtags to add headings and comments to your code.  
+
+
+## Deadline Saturday, June 15th, 2019, 11:55 pm
+
+## Load the the data set called chico.csv from the following location on GitHub
+# KPU-Statistics-2300 >> 2019 Spring >> data >> textbook_chico >> chico.csv
+
+## Generate a histogram, and describe the shape using concepts from Chapter 5 (skewness and kurtosis to draw conclusions about normality). 
+## Add your comments as hashtags
+
+
+## Generate a boxplot. Explain your observations of normality based on quantile measures.
+## Add your comments as hashtags
+
+
+## Compute the mean, the slow way
+
+
+## Double-check your work:
+
+
+## Centre the data (compute deviation scores)
+
+
+## Double-check your work:
+
+
+## Square each deviation score
+
+
+## Double-check your work:
+
+
+## Sum of Squares: Sum the squared deviation scores
+
+
+## Double-check your work:
+
+
+## Compute the variance
+
+
+## Double-check your work:
+
+
+## Compute the standard deviation
+
+
+## Double-check your work:
+
+
+## Compute the covariance of the two grades
+
+
+## Double-check your work
+
+
+## Compute the correlation coefficient
+
+
+## Double-check your work
+
+
+## Interpret the meaning of this value in the context of the data.
+
+
+## Save your work as an R script file, and submit it via Moodle. 
+
