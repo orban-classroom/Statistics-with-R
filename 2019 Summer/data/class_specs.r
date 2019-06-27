cc23d481bb6d798daa68205c1e4b2b04c0d543cb
Levente Orbán <llorban@users.noreply.github.com>
2019-06-27 11:41:44 -0700

    Update class_specs.r

--- 2019 Summer/data/class_specs.r
+++ 2019 Summer/data/class_specs.r
@@ -1,4 +1,4 @@
 class.specs <- data.frame(
-                    predominant.colour = c("green","blue","black","black","black","blue","black","pink","blue","black","grey","black","grey","green","blue","grey","red","pink","pink","green","black","black","black"),
+                    predominant.colour = c("green","blue","black","black","black","blue","black","pink","blue","black","grey","black","grey","green","blue","grey","red","pink","pink","green","black","black","black","grey"),
                     glasses = c(0,1,1,1,0,0,0,0,1,0,1,0,0,0,1,1,0,1,0,1,1,0,1,1)
                 )
