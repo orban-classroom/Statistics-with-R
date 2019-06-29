6b45b0bb05281bad47afde2ce6a5b48c38ebaafa
Levente Orbán <llorban@users.noreply.github.com>
2019-06-28 23:28:07 -0700

    Create 7-lab-homework.r

---
+++ 2019 Summer/R Lab Homework/7-lab-homework.r
@@ -0,0 +1,16 @@
+# Using the class.spec data frame we generated last class, answer the following questions: 
+#
+
+# What is the probability that someone with red predominant colour is selected from the class?
+
+# What is the probability that someone with red predominant colour wearing glasses is selected from the class?
+
+# What is the probability that someone with blue or black predominant colour is selected from the class?
+
+# Create a vector containing 10,000 randomly generated values of a white-noise distribution, values ranging from 10 to 100. 
+
+# Produce a histogram and learn to use arguments that we have not covered in class (use ?hist() to get more information). 
+
+# Create a vector containing 10,000 randomly generated values of a normal distribution, values ranging from 10 to 100.
+
+# Produce a histogram and use arguments that we have not covered in class (use ?hist() to get more information). 
