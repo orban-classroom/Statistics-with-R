3374d6d48b28b3205b549a3465c7c6e0f94ac87b
Levente Orbán <llorban@users.noreply.github.com>
2020-10-21 14:28:44 -0700

    Add files via upload

---
+++ 2020-summer/Exam1.R
@@ -0,0 +1,125 @@
+#########################
+##  EXAM 1, 110 points  ##
+#########################
+
+## GROUND RULES
+## ============
+## DO NOT DISCUSS THE EXAM WITH OTHERS. ANSWERS WILL BE MATCHED TO CATCH CHEATERS.
+## THIS IS AN OPEN-BOOK EXAM. YOU CAN LOOK AT YOUR TEXTBOOK AND COURSE VIDEOS, OR GOOGLE
+## YOU CAN WORK ON THE EXAM WHILE THE ASSIGNMENT MODULE IS OPEN.
+## THE SCRIPT YOU WRITE HERE MUST EXECUTE WITHOUT ERRORS FOR FULL MARKS. 
+## CHECK YOUR CODE FREQUENTLY AND REMOVE PROBLEM AREAS.
+## REMEMBER IF YOU MAKE A MISTAKE, YOU NEED TO RE-RUN ALL LINES OF CODE THAT DEPEND ON THE PROBLEMATIC LINE. 
+## 
+
+## GENERAL GRADING GUIDELINES
+## ==========================
+## CREATE VARIABLES WHEREVER APPROPRIATE
+## REUSE VARIABLES, DO NOT DUPLICATE COMPUTATIONS
+## REDUNDANT CODE WILL LOSE MARKS
+## UGLY DISORGANIZED WORK WILL LOSE MARKS
+## DO NOT LEAVE UNNECESSARY CODE IN THIS FILE
+##
+
+# # # # # # EXAM 1 BEGINS # # # # # # # #
+
+## ABOUT THE DATA
+## You will use the following data to complete this exam.
+## Execute the following line to load the data into this script:
+
+
+## RAW LINK: https://raw.githubusercontent.com/llorban/KPU-Statistics-2300/master/2019%20Spring/data/ubcgarbage/garbage_disposal.csv
+
+## 2016, Wu, DiGiacomo, Lenkic, Wong and Kingstone (UBC)
+## ABSTRACT: Previous observational work revealed that transient populations in a sustainable building disposed of 
+## waste more accurately when compared to patrons in a non-sustainable building. The current study uses an experimental 
+## design to replicate this observed effect and to investigate whether or not the built environment influences motivational 
+## factors to impact behavior. We find support that a building designed and built to communicate an atmosphere of sustainability 
+## can influence waste disposal behavior. Participants in the sustainable build- ing used the garbage receptacle significantly 
+## less and compensated by tending to select the containers and organics receptacle more, which actually resulted in more errors 
+## overall. Our findings suggest that building atmospherics can motivate people to recycle more. How- ever, atmospherics alone 
+## do not appear to be sufficient to elicit the desired performance outcome.
+
+## 1. Load any libraries needed for this notebook (2 points)
+
+
+## 2. Import the data as a data frame (2 points)
+
+
+## 3. What is the sample size? (2 points for the code that displays sample size, nothing else)
+
+
+## 4. What is the sum of all scores for each variables in the data frame? (4 points for the code that displays the sum of all scores, nothing else)
+
+
+## 5. What is the mean, the manual way, for both "proportion correct" and "time at bin" variables? (8 points, display the value)
+
+
+## 6. Use the built-in function to compute the mean for "proportion correct" and "time at bin" (4 points)
+
+
+## 7. What is the median of "proportion correct" and "time at bin" variables (2 points for the code that displays the values)
+
+
+## 8. What is the mode of "proportion correct" and "time at bin" variables (2 points for the code that displays the values)
+
+
+## 9. What is the range of scores of the "proportion correct" and "time at bin" variables?  (4 points for the code that displays the value)
+
+
+## 10. Compute the sums of squares for both "proportion correct" and "time at bin" variables, as described in my screencast (10 points for the code that displays the values)
+
+
+## 12. Compute the variance of both "proportion correct" and "time at bin" variables, the manual way (8 points for the code that displays the values)
+
+
+## 13. Verify your variance answers using the built-in variance function (4 points)
+
+
+## 14. Compute the standard deviation for both variables, the manual way (8 points points for the code that displays the values)
+
+
+## 15. Verify your answer using the built-in standard deviation function (4 points)
+
+
+## 16. Compute the correlation coefficient, r, between the "proportion correct" and "time at bin" variables. (4 points for the code that displays the value)
+
+
+## 17. What conclusion can you draw from the correlation answer in 15? (6 points, add comments as a haghtag remark)
+
+## 18. What is the standard score of the 3rd, 6th, and 9th value on the time at bin variable (6 points)?
+
+
+#################################################################################
+#################################################################################
+## DATASET 2
+## Raw Link: https://raw.githubusercontent.com/llorban/KPU-Statistics-2300/master/2019%20Spring/data/chimpstudy/chimpcookingstudy.csv
+##
+## 2015 Warneken and Rosati
+## Data Synposis: The transition to a cooked diet represents an important shift in human ecology and evolution. 
+## Cooking requires a set of sophisticated cognitive abilities, including causal reasoning, 
+## self-control and anticipatory planning. Do humans uniquely possess the cognitive capacities 
+## needed to cook food? We address whether one of humans' closest relatives, chimpanzees (Pan troglodytes),
+## possess the domain-general cognitive skills needed to cook. The percentage of cooked column in this data set
+## indicates the preference of chimpanzees for cooked food over raw food. Subject names refer to individual chimps.
+
+## Load the data into a dataframe (2 points)
+
+## What is the sample size? (2 points)
+
+## There is one variable with numeric data. Which one is that? (2 points)
+
+## What is the sum of all scores? (2 points)
+
+## What is the mean, the manual way, on the choice_cooked_percent column? (2 points)
+
+## What is the median on the choice_cooked_percent column? (2 points)
+
+## What is the mode of the choice_cooked_percent column?  (2 points)
+
+## What is the minimum, maximum and range of scores of the choice_cooked_percent column?  (6 points)
+
+## What is the interquartile range of choice_cooked_percent column? (4 points)
+
+## Conceptual: Based on your reading of the descriptive statistics, do you think chimps prefer 
+## cooked foods? Explain. (6 points)
