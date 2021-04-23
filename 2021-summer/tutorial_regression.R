7bda4441d66de61a6bf5b0535430e90298b4d05b
Levente Orbán <llorban@users.noreply.github.com>
2021-04-23 06:35:29 -0700

    Add files via upload

---
+++ 2021-summer/tutorial_regression.R
@@ -0,0 +1,23 @@
+# The Data (From Datadryad.org)
+zoo_data <- read.csv("https://raw.githubusercontent.com/llorban/KPU-Statistics-2300/master/2021-summer/zoodata.csv")
+
+# Peak at the data
+
+# Correlation
+
+# Regression: m x + a
+
+# Drawing Lines
+
+# DOING THE REGRESSION
+
+# Eye-balling method
+
+# Manual, slow Method
+
+# Slope
+
+# Intercept
+
+# Snappy Method
+
