ff643532b23641cc69bb65b2b0926c83ca3fd781
Levente Orbán <llorban@users.noreply.github.com>
2023-01-06 12:14:34 -0800

    Add files via upload

---
+++ 2023-spring/Homework/homework1.r
@@ -0,0 +1,52 @@
+
+# An addition
+5 + 5 
+
+# A subtraction
+5 - 5 
+
+# A multiplication
+3 * 5
+
+ # A division
+(5 + 5) / 2 
+
+# Exponentiation
+
+2^5
+# Modulo
+
+28 %% 6
+
+# Assign the value 42 to x
+x <- 42
+
+# Print out the value of the variable x
+x
+
+# Assign the value 5 to the variable my_apples
+
+my_apples <- 5
+
+# Print out the value of the variable my_apples
+
+my_apples
+
+# Assign a value to the variables my_apples and my_oranges
+my_apples <- 5
+my_oranges <- 6
+
+# Add these two variables together
+my_apples + my_oranges
+
+# Create the variable my_fruit
+my_fruit <- my_apples + my_oranges
+
+# Change my_numeric to be 42
+my_numeric <- 42
+
+# Change my_character to be "universe"
+my_character <- "universe"
+
+# Change my_logical to be FALSE
+my_logical <- FALSE
