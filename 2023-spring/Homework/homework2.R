efbbca27d61fd359addc9da5992f949e7a50a795
Levente Orbán <llorban@users.noreply.github.com>
2023-01-13 12:03:06 -0800

    Add files via upload

---
+++ 2023-spring/Homework/homework2.R
@@ -0,0 +1,32 @@
+# R Homework 2
+
+# Follow these stylistic conventions
+# 1. Have your own system of variable naming, and stick to it. I will be looking for consistency.
+# 2. Use a single hashtag # for your comments. I will use ## for my comments.
+# Use spaces carefully to organize your code. Make it readable. You can use hashtags to add headings and comments to your code.  
+
+examdata <- c(95.57, 74.91, 90, 74.16, 59.02, 80.67, 81.58, 82.39, 0, 81.93, 81.84, 88.9, 0, 76.33, 66.68, 75.87, 96.96, 77.44, 69.46, 90.82, 85.87, 94.71, 84.25, 62.26, 45.84, 72.21, 67.44, 86, 90.5, 96.69, 76.17, 96.41, 37.53, 119, 97, 57.64, 89.79, 77.01, 79.62, 76.92, 71.35, 41.57, 45.95, 86.92, 46.08, 74.2, 47.31, 98, 106.76, 0, 74.31, 51.79, 0, 100.29, 108.69, 77.51, 64.62, 93.69, 62.87, 0)
+
+# Compute the mean of the examdata data set manually, using the formula.
+
+
+# Compute the mean of the examdata data using the R function. 
+
+
+# Compute the median of examdata using the R function. 
+
+
+# Compute the mode of each of the two variables in the vector.
+# Use multiple different bin sizes to characterize the mode.
+
+# What is the minimum value in this data set?
+
+
+# What is the maximum value in this data set?
+
+
+# What is the range of this data set?
+
+
+# What is the interquartile range of this data set?
+
