d833141e78b0c0b89fbaa6379fd0a42f2eca7ea5
Levente Orbán <llorban@users.noreply.github.com>
2023-01-23 13:32:00 -0800

    Add files via upload

---
+++ 2023-spring/Homework/homework3.R
@@ -0,0 +1,38 @@
+
+## Homework 3
+## ##########
+
+## 1.
+## Import the 2300data.csv data set from the 2023-spring GitHub folder and assign it to a variable
+## Be sure to use the raw link from GitHub
+## 
+
+code goes here, delete this remark
+
+## 2. 
+## Create two filtered variables, one filtering for 2019-20 (2019 summer semester) and one for your class (2023-10).
+## The filtered data should contain amount of coffee consumed per week. 
+
+code goes here, delete this remark
+
+## 3.
+## Compute the mean of each new variable you created
+
+code goes here, delete this remark
+
+## Compute the median of each new variable you created
+
+code goes here, delete this remark
+
+## Generate 2 histograms (one for each variable) and judge the mode of the curve.
+
+code goes here, delete this remark
+
+## State your answer of the mode below as a remark (with hashtag)
+
+## 4.
+## Generate two boxplots of the variables
+## Comment on the differences between the two plots
+## You can use "pre & post covid" to characterize the two classes, etc
+
+code goes here, delete this remark
