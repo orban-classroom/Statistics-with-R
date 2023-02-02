116285b1fbc09590d152694c6d6668f221f0fea3
Levente Orbán <llorban@users.noreply.github.com>
2023-02-02 09:05:02 -0800

    Create homework4.r

---
+++ 2023-spring/Homework/homework4.r
@@ -0,0 +1,39 @@
+## Homework 3
+## ###########
+## Homework 4
+## ###########
+
+## 1.
+## Import the UBC Garbage study data set: https://raw.githubusercontent.com/orban-classroom/Statistics-with-R/master/2019%20Spring/data/ubcgarbage/garbage_disposal.csv
+## The study describes people throwing away garbage in a sustainable and a non-sustainable building.
+## 
+
+code goes here, delete this remark
+
+## 2. 
+## Create two filtered variables showing "proportion_correct" values, based on each condition of the study using the "condition" column.
+## 
+
+code goes here, delete this remark
+
+## 3.
+## Compute the mean of each new variable you created
+
+code goes here, delete this remark
+
+## Compute the median of each new variable you created
+
+code goes here, delete this remark
+
+## Generate 2 histograms (one for each variable) and judge the mode of the curve.
+
+code goes here, delete this remark
+
+## State your answer of the mode below as a remark (with hashtag)
+
+## 4.
+## Generate two boxplots of the variables
+## Comment on the differences between the two plots
+## You can use "pre & post covid" to characterize the two classes, etc
+
+code goes here, delete this remark
