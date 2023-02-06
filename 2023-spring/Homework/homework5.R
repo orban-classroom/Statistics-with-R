9bbfa968708d6b17a67f794ed21ff9a1964f49d2
Levente Orbán <llorban@users.noreply.github.com>
2023-02-06 14:18:49 -0800

    Add files via upload

---
+++ 2023-spring/Homework/homework5.R
@@ -0,0 +1,29 @@
+## Homework 5
+
+## In this homework, you will work with a new dataset that describes various 
+## mental health metrics of university students from Japan. Skim PDF to get an idea:
+## https://github.com/orban-classroom/Statistics-with-R/blob/master/2023-spring/data/studentmentalhealth/pone.0218357.pdf
+
+## Import the data from Table 1: 
+## https://raw.githubusercontent.com/orban-classroom/Statistics-with-R/master/2023-spring/data/studentmentalhealth/Sheet%201-Table%201.csv
+
+# 1. Compute the standard deviation of the following two variables: 
+## WAIS_FIQ (their IQ scores)
+## HR..mean. (the average heart rate during the study)
+
+## Show part calculations for sample size, degrees of freedom, mean, deviation scores, sum of squares, and variance
+## Verify your answers wherever possible
+## Tips: Avoid redundant code, (2) assign part calculations to variables, (3) do not include unnecessary code
+
+
+# 2. Compute the covariance between the two variables, WAIS_FIQ and HR..mean.
+## In essence, we are asking the research question, is anxiety, as measured by heart rate 
+## associated with lower or higher scores on the WAIS intelligence scale?
+
+## Show part calculations: deviation scores, covariance
+## More Tips: (1) Use variables you defined in part 1
+
+# 3. What is the direction of the relationship between intelligence scores and heart rate?
+
+
+# 4. Why can you not make a comment about the magnitude of the relationship?
