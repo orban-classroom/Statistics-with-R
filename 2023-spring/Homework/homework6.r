c2e27aab9899bc3d37abf4b86c0a5764134446b1
Levente Orbán <llorban@users.noreply.github.com>
2023-02-13 11:16:49 -0800

    Create homework6.r

---
+++ 2023-spring/Homework/homework6.r
@@ -0,0 +1,17 @@
+## Homework 6
+
+## For this homework, you will use a new data file called PSYC 1100 exam data. 
+## This file contains midterm and final exam scores for several PSYC 1100 courses a few years ago. 
+## Raw Github link: https://raw.githubusercontent.com/orban-classroom/Statistics-with-R/master/2019%20Spring/data/1100examdata/1100_exam_data.csv
+
+## Research question: What is the relationship between midterm and final exam grades? Is a student who does well on a midterm also likely to do well on a final exam?
+
+## Conduct a correlation analysis, manually. 
+## Also create a boxplot, and a scatter plot with a regression line. 
+
+## Tips: 
+## Show all work, assign variables where it makes sense
+## Remove unnecessary scrap code
+## Do not hardcode numbers anytime
+## Remove redundant code and duplicate variables
+## Use headings and narrate your notebook
