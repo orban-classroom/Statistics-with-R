dae543d20956e321fac47efe4b099e2b63d0c087
Levente Orbán <llorban@users.noreply.github.com>
2023-03-17 10:14:00 -0700

    Update and rename homework8.R to homework7.R

--- 2023-spring/Homework/homework7.R
+++ 2023-spring/Homework/homework7.R
@@ -2,7 +2,7 @@
 
 # Use the PSYC 2300 data: https://raw.githubusercontent.com/orban-classroom/Statistics-with-R/master/2023-spring/data/2300data.csv
 
-# Compare student 67 and student 68's sleep habits. Convert their scores to 
+# Compare student 66 and student 67's sleep habits. Convert their scores to 
 # z-scores and make some comments about how extreme their scores are. 
 # Follow the calculation sequence presented in lecture, and use 
 # the comparison to the normal curve as displayed in the lecture slides
