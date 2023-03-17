4c2460e0f1f8fa01440593115952cf3c9c35839a
Levente Orbán <llorban@users.noreply.github.com>
2023-03-17 12:16:05 -0700

    Add files via upload

---
+++ 2023-spring/Homework/homework8.R
@@ -0,0 +1,18 @@
+# Homework 8, Week 11
+
+# Use the mental health student data for this exercise
+# Raw link: https://raw.githubusercontent.com/orban-classroom/Statistics-with-R/master/2023-spring/data/studentmentalhealth/Sheet%201-Table%201.csv
+
+## Split the data by sex, and use the "S_score STAI State Anxiety Score" for your calculations
+
+## Create two vectors containing "S_score STAI State Anxiety Score" split by sex
+
+## Compute each mean and store the two means in a single vector and assign that to a variable
+
+## Compute each SEM and store them in a single vector, assigned to a variable
+
+## Design a barplot graph, and store the mid points in a variable
+
+## Develop an arrows code plotting the SEM
+
+## Verify that you are getting a reasonable output
