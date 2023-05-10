bf2e6a7a7587cc4c6ae2046c2e56c2b659549776
Levente Orbán <llorban@users.noreply.github.com>
2023-05-10 16:13:00 +1200

    Create homework_1.r

---
+++ 2023-summer/Homework/homework_1.r
@@ -0,0 +1,40 @@
+# Homework 1
+
+# Instructions
+# Download this R Script and load it in your copy of R Studio
+# Enter your answers as code below each question
+# Save your work and upload to Moodle by the deadline
+# Post screenshots of errors of the entire R Studio screen (or take a photo of it)
+# on the Signal classroom group. Do not private message me on technical questions
+# because I would like everyone to troubleshoot together. 
+
+# Decide on a variable naming scheme for your variables.
+# Using a hashtag comment below, state your naming format
+
+# Create a variable with the number 10. 
+
+# Create a second variable with the number 52 (which is the meaning of life, if you watched Hitchiker's Guide to the Galaxy)
+
+# Add the two numbers together using their variable names
+
+# Subtract the second variable from the first variable.
+
+# Functions are built-in commands on R that do some internal computation
+# For example, the function rm() deletes a variable from the global environment
+# Delete the second variable from the global environment
+
+# When you don't understand how a function works, you can type in
+# the function's name with a question mark before it, like so ?rm()
+# Type ?c() and read the help document (bottom right quadrant)
+
+# Create a vector using c() that contains last week's BC lottery winning numbers (Google it)
+
+# In R, you can create different data types. Until now you have been dealing with one type
+# Using the class() function, find out what kind of data have you been dealing with
+# Figure it for the first variable (the second one has been deleted), and for the vector your created.
+
+
+
+
+# end of homework
+##################
