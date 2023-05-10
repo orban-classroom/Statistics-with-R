78f76bedde4ceb50a406522a55740940f975d541
Levente Orbán <llorban@users.noreply.github.com>
2023-05-10 15:50:53 +1200

    Create lecture1_as_delivered.r

---
+++ 2023-summer/LectureNotes/lecture1_as_delivered.r
@@ -0,0 +1,30 @@
+# Basic Operations
+1 + 1
+1 - 1
+1 * 1
+1 / 1
+1^2
+ 1 + 1
+1+1 
+1+ 1
+1 = 1. # single equal sign means "make it equal to xxxx" 
+1 == 1 # is it equal to?
+2 == 1
+
+# Variables
+
+MYFAVOURITENUMBER <- 5
+
+myfavouritenumber <- 5
+myfavouritenumber + 111
+my favourite number <- 5 # variable name cannot contain spaces
+my.favourite.number <- 5
+my_favourite_number <- 5
+my-favourite-number <- 5 # variable name cannot contain dashes
+MyFavouriteNumber <- 5
+MYFAVOURITENUMBER <- 10
+myFavouriteNumber <- 5 # capitalization is very important
+
+rm(my_favourite_number)
+
+x <- 5 # make your variable names informative
