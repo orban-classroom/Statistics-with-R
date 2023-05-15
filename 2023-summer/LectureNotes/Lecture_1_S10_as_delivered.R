02f1a6f939e8bfe34ec268163b017146934d9a47
Levente Orbán <llorban@users.noreply.github.com>
2023-05-16 10:11:22 +1200

    Add files via upload

---
+++ 2023-summer/LectureNotes/Lecture_1_S10_as_delivered.R
@@ -0,0 +1,43 @@
+## Basic Operations
+
+1 + 1
+# activate the code: 
+# Cmd + Return on a Mac
+# Ctrl + Enter on a Windows
+
+1 - 1
+1 * 1
+1 / 1
+1^1 ## raising it to the power of. Exponentiation
+
+# A word about equal signs
+1 = 1 ## 1 equal sign means, "make it equal to..."
+1 == 1 ## double-equal sign means, "is it equal to?"
+1 == 2
+1 < 2
+1 > 2
+1 != 1 ## is it not equal to?
+1 != 2
+
+## Concept of variables
+## containers of information: numbers, text, data, etc
+
+myfavouritenumber <- 9 
+
+myfavouritenumber
+
+myfavouritenumber + 1000
+
+
+## Functions
+
+## function called rm to delete single variables
+rm(myfavouritenumber)
+
+## if you don't know how to use a function, 
+## Ask R to tell you
+
+?rm()
+
+
+
