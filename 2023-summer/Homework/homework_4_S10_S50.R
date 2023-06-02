339245c8f91c38234ee92d761f702e0be22d8b06
Levente Orbán <llorban@users.noreply.github.com>
2023-06-02 22:15:04 +1200

    Create homework_4_S10_S50.R

---
+++ 2023-summer/Homework/homework_4_S10_S50.R
@@ -0,0 +1,43 @@
+# Homework 4
+############
+
+# Complete the 2300 expansion survey: https://orban.wufoo.com/forms/q1npgx7x1d69b27/
+
+
+# Find the URL for the PSYC 2300 data from the 2023 summer : data folder
+
+
+# Use the hrs.screentime.weekly variable for this homework. 
+
+
+# Create a variable/object containing the imported data
+
+
+# Create a variable filtering for all rows containing 201910 (2019 Spring) semester
+
+
+# Create a variable filtering for all rows containing 202310 (2023 Spring) semester
+
+
+# Compute the mean, median and mode for each 201910 and 202310 data separately, below, using the manual method
+
+
+# Comment on your observations of the central tendency measures. Comments both in terms of differences between mean, median and mode numbers and what they reveal about the data, and differences between the two semesters. 
+
+
+# Create a histogram of each semester's data. Make it look fancy: include labels, colour it nicely, dress it up.
+
+
+# Comment on the shape of the histogram. Use expressions such as skew and kurtosis (based on your visual inspection). 
+
+
+# Create two boxplots side by side containing each semester's data. Make it look fancy: include labels, colour it nicely, dress it up.
+
+
+# Comment on the appearance and differences between the boxplots. What do these box plots reveal about the data?
+
+
+# Compute the variance and standard deviation of each semester's data. 
+
+
+# Comment on the standard deviation values. 
