09ed4e09295347d6715e88caa432ef0652398a94
Levente Orbán <llorban@users.noreply.github.com>
2023-07-15 18:07:27 +1200

    Add files via upload

---
+++ 2023-summer/Homework/homework_10_S10_S50.r
@@ -0,0 +1,23 @@
+# Using the 2023 version of the PSYC 2300 Stats Class data, answer the following questions: 
+# Let's assume for this exercise, that you are dealing with the population of all Kwantlen stats students.
+# Raw link: https://raw.githubusercontent.com/orban-classroom/Statistics-with-R/master/2023-summer/Data/2300_2023.csv
+
+# Combine your knowledge of filters/subsetting with the equation shown in class to compute probabilities for the problems below.
+# You may use filters using the square bracket sequence [ xxx ], functions such as length, and combining filter rules using &, ==
+
+# What is the probability that someone does no exercise at all in a week?
+
+# What is the probability that someone is sleep deprived (assuming 8 hours/day x 7 days a week as normal)?
+
+# What is the probability that someone consumes no caffeine at all in stats courses?
+
+# ##############
+# Define two groups of students: those who are currently enrolled in the course (Semester: 202320), and all those who took the course in the past (less than 202320)
+
+# If you find someone who drinks no caffeine at all, are they more likely to be present students or past students?
+
+# If you find someone who is exposed to over 56 hours of screen time per week, are they more likely to be present students or past students?
+
+# If you find someone who is sleep deprived (as per earlier definition), are they more likely to be present students or past students?
+
+
