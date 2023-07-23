27a358519d502d1006780f87196e69e7f76cded4
Levente Orbán <llorban@users.noreply.github.com>
2023-07-23 15:48:02 -0700

    Add files via upload

---
+++ 2023-summer/Homework/homework_11_S10_S50.r
@@ -0,0 +1,11 @@
+## Optional Homework, Week 11
+## Data Set: https://raw.githubusercontent.com/orban-classroom/Statistics-with-R/master/2019%20Spring/data/1100examdata/1100_exam_data.csv
+
+
+## 1. Conduct assumptions checking
+## 2. Make a decision on the type of t-test required
+## 3. Implement the appropriate t-test using the manual method
+## (Concepts prior to Take-Home Exam 1 can be automatized.)
+
+## Research Question: Are PSYC 1100 students' performance signiciantly different between exam 1 (first column) and exam 2 (second column?
+## Note: Each row represents an individual student's performance on two exams. 
