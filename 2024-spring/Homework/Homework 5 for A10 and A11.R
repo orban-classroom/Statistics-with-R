9b04d16624c48e873520d06fff5b24f1a4b8673f
Levente Orbán <llorban@users.noreply.github.com>
2024-02-09 18:38:54 -0800

    data disgust sensitivity to 2024-10

---
+++ 2024-spring/Homework/Homework 5 for A10 and A11.R
@@ -0,0 +1,44 @@
+## Homework 4
+############
+
+## Load the PSYC2300 class survey data here.
+## You can find the raw GitHub link in the usual location.
+
+
+## Use the dependents variable for this homework. 
+
+
+## Create two variables, one containing dependents of section A10 and one for A11. 
+## Also, filter out blank values.
+
+
+## Use the A10 data for this section of the calculations: 
+
+
+## Compute the mean. Break out any calculation into a variable if you plan to 
+## reuse the value later (e.g., sample size). Verify your work
+
+
+## Compute the sums of squares. Make diagnostic remarks (indicators that indicate \
+## you are on the right track)
+
+
+## Compute the variance. Verify your calculation
+
+
+## Compute the standard deviation. Verify your calculation
+
+
+## Repeat the same steps for A11
+
+
+
+## Use the method described in this Stack Overflow article to produce two histograms 
+## blended together to show the number of dependents in each of the classes: 
+## https://stackoverflow.com/questions/3541713/how-can-i-plot-two-histograms-together-in-r
+## The answer of interest is posted by "Dirk is no longer here"
+
+
+
+
+
