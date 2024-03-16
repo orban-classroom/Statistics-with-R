ffacfd5f8c30c918838001afb8342f1cc9a69199
Levente Orbán <llorban@users.noreply.github.com>
2024-03-15 22:10:48 -0700

    homework 6

---
+++ 2024-spring/Homework/Homework 6 for A10 and A11.r
@@ -0,0 +1,23 @@
+# Homework 6
+
+# Use the PSYC 2300 Stats Class data and answer the following questions: 
+
+# Combine your knowledge of filters/subsetting with the equation shown in class to compute probabilities for the problems below.
+# You may use filters using the square bracket sequence [ xxx ], functions such as length, and combining filter rules using &, ==
+
+# What is the probability that someone does no exercise at all in a week?
+
+# What is the probability that someone is sleep deprived (assuming 8 hours/day x 7 days a week as normal)?
+
+# What is the probability that someone consumes no caffeine at all among stats students?
+
+# ##############
+# Define two groups of students: those who are currently enrolled in the course (Semester: 202410), and all those who took the course in the past (less than 202410)
+
+# If you find someone who drinks no caffeine at all, are they more likely to be present students or past students?
+
+# If you find someone who is exposed to over 56 hours of screen time per week, are they more likely to be present students or past students?
+
+# If you find someone who is sleep deprived (as per earlier definition), are they more likely to be present students or past students?
+
+
