c295246e62378069c1f6f06216a98d75cb7e77ae
Levente Orbán <llorban@users.noreply.github.com>
2024-03-23 23:25:15 -0700

    Homework 7

---
+++ 2024-spring/Homework/Homework 7 for A10 and A11.R
@@ -0,0 +1,53 @@
+## Homework 7
+
+## You will work with a new data set called "studentwellbeing_nice.csv" in the 2024/Data folder
+## This data contains a mental health survey of students from Nice, France
+## You will work with two variables: 
+
+## Research questions: 
+## (1) Are anxiety symptoms associated with significant differences in body weight? 
+## (2) Are learning disabilities associated with significant differences in body weight?
+
+## Load the Data
+
+
+
+## Filter for the variables under study: 
+
+
+
+## Check the assumptions we covered in class
+
+
+
+## Compute the SEM
+
+
+
+## Load SEM's into a single vector
+
+
+## Compute the means and load them into a single vector
+
+
+
+## Generate a barplot and save their graphical mid-points as a variable
+
+
+
+## Draw the bar plot of the means
+
+
+
+
+## Draw the error bar on the bar plot indicating the SEM
+
+
+
+
+## Draw your conclusions
+
+
+
+
+## end of homework
