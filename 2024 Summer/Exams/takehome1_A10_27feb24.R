c9333d17f632c3403bd41b94c939de9b7a167fb4
Levente Orbán <llorban@users.noreply.github.com>
2024-06-04 12:13:50 +1200

    exam L10

---
+++ 2024 Summer/Exams/takehome1_A10_27feb24.R
@@ -0,0 +1,77 @@
+
+
+## TAKE-HOME EXAM 1
+## PSYC 2300 - APPLIED STATISTICS 1
+## WINDOW: Feb 27 - 29
+## INTENDED DURATION: 2 hours (not monitored)
+
+## EXAM REGULATIOMS: 
+## 1. Do not discuss the exam with anyone.
+## 2. You can message Levente with any questions, including feedback on homework performance to assist with answers on this exam.
+## 3. You may use resources such as the textbook, lecture slides, lecture R notes and homework to complete this exam. 
+## 4. Do not talk to classmates. 
+## 5. Do not talk to tutors, on online forums or any other means
+## This is an exam. Rule violations will be investigated by the dean's office for academic integrity violation. 
+
+## About the Data Set: 
+## The data set describes chimpanzees' risky behaviours and correlates to their 
+## stress levels as measured by the presence of cortisol hormone, and their 
+## testosterone levels, among other variables. 
+
+## File name on GitHub: Rosati_etal_ChimpanzeeAdolescence-Data_TaskRelations
+
+
+## Variables you will use are: "RiskAffectScore", "Cortisol", "Testosterone"
+
+
+
+## NORMALITY - 30% 
+## Assess each of the three variables for normality. Draw on your of knowledge of normal curves, 
+## skew, kurtosis, outliers, variability to make your conclusions. Use technical language (as per textbook) and visualizations.
+## such as histograms and boxplots to support your arguments. 
+## Keep your work clean with headings and comment your code.
+
+
+
+
+
+
+
+
+## CENTRAL TENDENCIES - 20%
+## Where is the middle of the data -- for each of the three variables?
+## Justify your answer using concepts of normality, and measures of central tendencies. 
+## All calculations using the manual method (but you can use built-in functions for verification)
+## Keep your work clean with headings and comment your code.
+
+
+
+
+
+
+## Concept question: can the mean, median and mode be identical for a data set?
+
+
+
+## VARIABILITY - 30%
+## Compute variance and standard deviation of each of the three variables.
+## Keep your work clean with headings and comment your code. 
+## Compute interim variables (deviation scores, sums of squares)
+
+
+
+
+
+
+## CORRELATIONS - 20%
+## Compute the covariance and correlationship between the following variables: 
+## RiskAffectScore x Cortisol
+## RiskAffectScore x Testosterone
+## Describe the relationship between each of those variables
+
+
+
+
+
+
+
