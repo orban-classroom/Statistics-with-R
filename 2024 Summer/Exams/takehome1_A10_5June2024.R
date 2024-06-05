22b7473aaa80150b79e0c0c854456bc240490d0f
Levente Orbán <llorban@users.noreply.github.com>
2024-06-06 08:07:10 +1200

    exam a10

---
+++ 2024 Summer/Exams/takehome1_A10_5June2024.R
@@ -0,0 +1,99 @@
+
+
+## TAKE-HOME EXAM 1
+## PSYC 2300 - APPLIED STATISTICS 1
+## WINDOW: June 4 - 7
+## INTENDED DURATION: 2 hours
+
+## EXAM REGULATIOMS: 
+## 1. Do not discuss the exam with anyone.
+## 2. You can message Levente with any questions, including feedback on homework performance to assist with answers on this exam.
+## 3. You may use resources such as the textbook, lecture slides, lecture R notes and homework to complete this exam. 
+## 4. Do not talk to classmates. 
+## 5. Do not talk to tutors, on online forums or any other means
+## This is an exam. Rule violations will be investigated by the dean's office for academic integrity violation. 
+
+## About the Data Set: 
+## The data set describes chimpanzees' risky behaviours and correlates to their 
+## stress levels as measured by the presence of cortisol hormone, and their 
+## testosterone levels, among other variables. 
+
+## File name on GitHub: "takehome1_Rosati_etal_ChimpanzeeAdolescence-Data_TaskRelations.csv"
+
+
+
+## Variables you will use are: "RiskAffectScore", "Sex"
+
+
+
+
+## Research Question
+## Are there sex differences in risk taking as measured by the RiskAffectScore variable?
+
+## FILTER - 10%
+## Create a filter that separates the data into male and female groups.
+
+
+
+
+## DISTRIBUTION SHAPE - 30% 
+## Create a combined histogram plotting the RiskAffectScore variable for the 
+## males and females. Use guidance from homework4 to plot two histograms together
+## Make sure the histograms have equal width
+
+
+
+
+
+## Concept. Describe the visual appearance of the distribution's shape based on 
+## expressions such as kurtosis and skewness (see textbook). 
+
+
+
+
+## Concept. Does anything stand in terms of the extent of risk affect scores
+## between males and females (outliers, range, min, max)
+
+
+
+
+
+## CENTRAL TENDENCIES - 20%
+## Where is the middle of the data -- for male vs female risk affect scores?
+## Justify your answer using concepts of normality, and measures of central tendencies. 
+## All calculations using the **manual method**
+## Use the built-in functions to verify your work.
+## Keep your work clean with headings and comment your code.
+
+
+
+
+
+
+## Concept question: can the mean, median and mode be identical for a data set?
+
+
+
+
+
+
+## VARIABILITY - 40%
+## Compute the sums of squares, variance and standard deviation of each of the variables.
+## Keep your work clean with headings and comment your code. 
+## Compute interim variables (deviation scores, sums of squares)
+
+
+
+
+
+
+
+
+## Report the means and standard deviations below
+## What is your assessment? Does it look like there are differences in
+## risk affect score given the amount of variability in the data?
+
+
+
+
+
