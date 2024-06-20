3fdf7d1b3feb13f00562ccdfab9847d548dbce30
Levente Orbán <llorban@users.noreply.github.com>
2024-06-20 14:13:35 -0700

    hw 8

---
+++ 2024 Summer/Homework/Homework 8 for A10 and L10.R
@@ -0,0 +1,51 @@
+## Homework 8
+##############
+
+## You will use a new data set for this homework, dealing with disgust sensitivity
+## The data package and description if doung in the Data/disgustsensitivity folder.
+## Read about the data set by looking at the "10-91-1-PB.pdf" file.
+
+## Load the disgust sensitivity data from the GitHub folder:
+## Load the data file: Berger_and_Anaki_disgust_scale_2014_1_comma_delimited_cleaned.csv
+
+## You will deal with two variables: `Age`, `Mean_general_ds`
+
+## FILTER
+## Create a filter that you can call "analysis_dataframe" or something similar that matches your
+## variable naming style, and isolate the two variables in question while also removing potential NA cells.
+
+
+## PRE-CHECKS
+## Generate box-plots and histograms of each of the variables 
+## Compute skewness and kurtosis values.
+## Comment on normality (as per textbook definitions)
+
+
+
+
+## QUESTION 1
+## Compute the covariance between `Age` and `Mean_general_ds`?
+## Show your work manually. Verify using built-in function and run diagnostics along the way.
+
+
+
+
+
+
+## QUESTION 2
+## Standardize the two variables and compute the correlation coefficient manually.
+## Describe your findings
+
+
+
+
+
+## Generate a scatterplot of the variables.
+## Comment on the appearance. 
+
+
+
+
+
+
+## end of homework
