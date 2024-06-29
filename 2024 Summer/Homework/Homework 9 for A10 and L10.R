eb714ee17a61a13fb3bf43089c824fc2be908eb1
Levente Orbán <llorban@users.noreply.github.com>
2024-06-28 19:28:42 -0700

    stressdata

---
+++ 2024 Summer/Homework/Homework 9 for A10 and L10.R
@@ -0,0 +1,54 @@
+## Homework 9
+##############
+
+
+
+
+
+## FILTER
+## Create a filter that you can call "analysis_dataframe" or something similar that matches your
+## variable naming style, and isolate the two variables in question while also removing potential NA cells.
+
+
+
+
+
+## PRE-CHECKS
+## Generate box-plots and histograms of each of the variables 
+## Compute skewness and kurtosis values.
+## Comment on normality (as per textbook definitions)
+
+
+
+
+## QUESTION 1
+## Compute the covariance between `Age` and `Mean_general_ds`?
+## Show your work manually. Verify using built-in function and run diagnostics along the way.
+
+
+
+
+
+
+## QUESTION 2
+## Standardize the two variables and compute the correlation coefficient manually.
+## Describe your findings
+
+
+
+
+
+## Generate a scatterplot of the variables.
+## Comment on the appearance. 
+
+
+
+
+
+
+## Generate a barplot and add error bars representing SEM.
+
+
+
+
+## end of homework
