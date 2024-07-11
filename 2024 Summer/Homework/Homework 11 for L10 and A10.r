01877ef5c002b783d1cecffe2d170c2d39baefd8
Levente Orbán <llorban@users.noreply.github.com>
2024-07-11 09:06:42 -0700

    Homework 11

---
+++ 2024 Summer/Homework/Homework 11 for L10 and A10.r
@@ -0,0 +1,17 @@
+## Homework, Week 11
+## Data Set: 1100_exam_data_independent
+
+## Research Question: Are PSYC 1100 students' performance significantly different between exam 1 and exam 2?
+
+## 1. Load relevant libraries
+## 2. Filter the data
+## 3. Sanitize the data
+## 3. Conduct assumptions checking
+## 3. a) Normality
+## 3. b) Homogeneity of variance
+## 4. Compute variances, including pooled variance
+## 5. Compute pooled standard error of mean
+## 6. Conduct independent samples t-test
+## 7. Compute critical t-value, and exact p-value
+## 8. Interpret results
+## 9. Create a bar chart showing means, and add error bars indicating standard errof of the mean (not pooled)
