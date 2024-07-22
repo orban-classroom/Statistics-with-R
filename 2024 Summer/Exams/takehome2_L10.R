0fade2763fc1077c156a14d3269ac6ed875e86e7
Levente Orbán <llorban@users.noreply.github.com>
2024-07-22 16:00:44 -0700

    Take Home Exam 2 for L10

---
+++ 2024 Summer/Exams/takehome2_L10.R
@@ -0,0 +1,108 @@
+# TAKE-HOME EXAM 2
+#
+#
+#################################################################################
+#################################################################################
+## PREAMBLE
+## You can use your notes, textbook and web during the exam.
+## COMMUNICATIONS EMBARGO: Communication either with a classmate, over the phone or over the web is absolutely forbidden. 
+## Keep your work concise and clean: add your answer below the headings
+## Eliminate redundant lines of code
+## All of your interim calculations must be done manually except for Take-Home Exam 1 content
+## Tip: Show that you have double checked your work
+## All of your answers must be supported by calculations for full points. 
+## Partial points can be achieved even with errors.
+## Answer conceptual questions with a hashtag comment. For full marks consider lecture discussions.
+
+#################################################################################
+#################################################################################
+
+# PROBLEM 1
+# Data: takehome2_Data_India_20150409
+# About: Recent studies suggest that cooperative decision-making in one-shot interactions 
+# is a history-dependent dynamic process: promoting intuition versus deliberation typically 
+# has a positive effect on cooperation (dynamism) among people living in a cooperative setting 
+# and with no previous experience in economic games on cooperation (history dependence). 
+# Here, we report on a laboratory experiment exploring how these findings transfer to a 
+# non-cooperative setting. We find two major results: (i) promoting intuition versus 
+# deliberation has no effect on cooperative behaviour among inexperienced subjects living 
+# in a non-cooperative setting; (ii) experienced subjects cooperate more than inexperienced 
+# subjects, but only under time pressure. These results suggest that cooperation is a learning 
+# process, rather than an instinctive impulse or a self-controlled choice, and that experience 
+# operates primarily via the channel of intuition. Our findings shed further light on the 
+# cognitive basis of human cooperative decision-making and provide further support for the 
+# recently proposed social heuristics hypothesis.
+
+
+# Using this data set, the authors have examined the effect of time pressure on cooperation.
+# You will do a different analysis on this data set, examining whether biological sex (column name: sex) has
+# any effect on self-perceived pressure (column name: pressure)
+
+# Research question: Filtering the data for those in the "pressure" condition, is there a significant difference between males
+# and females on the "time pressure" variable? Conduct a t-test to find out.
+
+# Import the data from GitHub, exams folder "takehome2_Data_India_20150409.csv"
+
+
+# Filter the data for the relevant variables and rows.
+
+
+# Sanitize the data
+
+
+# Assumptions checks
+
+
+# What kind of t-test will you implement?
+
+
+# Implement a manual t-test calculation. Verify using the built-in function
+
+
+# Reach a statistical and research conclusion.
+
+
+# PROBLEM 2
+# doi: 10.5061/dryad.bcc2fqz92
+# Data: takehome2_exercise_memory_data
+# Background: The inconsistent results of the studies on whether acute exercise 
+# can improve working memory and the small effect sizes of meta-analysis indicate 
+# that there may be some moderating variables affecting the intensity of the 
+# relationship between acute exercise and working memory. This research examined 
+# the effects of different intensity acute exercise on the two types of working 
+# memory.
+#   Seventy-eight low-active college students aged 18 to 22 years were 
+# selected and randomly assigned to four groups. The low-intensity group (n=19), 
+# moderate-intensity group (n=19) and high-intensity group (n=19) completed 
+# 30-min cycle ergometer exercise bouts separately at the intensity of 40%, 
+# 60% and 80% of Vo2max, while the control group (n=21) read for 30-min. The 
+# delayed match-to-sample test was used to test verbal and spatial working 
+# memory after the intervention.
+
+# Your Research Question: Is there a significant difference between the 
+# low intensity (LG coded as 1) and the high intensity groups (HG coded as 3) in 
+# terms of spatial-reasoning working memory accuracy (Accuracy_SpatialMem). 
+
+
+# Import the data from GitHub, exams folder ""
+
+
+# Filter the data for the relevant variables and rows.
+
+
+# Sanitize the data
+
+
+# Assumptions checks:
+
+
+# What kind of t-test will you implement?
+
+
+# Implement a manual t-test calculation. Verify using the built-in function
+
+
+# Reach a statistical and research conclusion.
+
+
+
