c9858abc25bd6f70d82c90c0b4f833a7975c5d7f
Levente Orbán <llorban@users.noreply.github.com>
2025-02-04 16:05:44 -0800

    Take-home exam 1 for A11

---
+++ 2025 Spring/Exams/takehome1-a11.R
@@ -0,0 +1,86 @@
+## TAKE-HOME EXAM 1
+## PSYC 2300 - APPLIED STATISTICS 1
+## INTENDED DURATION: 2 hours (not monitored)
+
+## EXAM REGULATIONS: 
+## 1. Do not discuss the exam with anyone.
+## 2. You can message Dr. Orban with any questions, including feedback on homework performance to assist with answers on this exam.
+## 3. You may use resources such as the textbook, lecture slides, lecture R notes and homework to complete this exam. 
+## 4. Do not talk to classmates. 
+## 5. Do not talk to tutors, on online forums or any other means
+## This is an exam. Rule violations will be investigated by the dean's office for academic integrity violation. 
+
+## About the Data Set: 
+## The data set describes chimpanzees' personality
+## File name on GitHub (inside the "Exams" folder): Rawlings_et_al_chimp_LT_personality_data.csv
+
+## Variables you will use are: "Sex", "T2 dominance"
+
+
+## NORMALITY
+## Assess `T2 dominance` for normality. Draw on your of knowledge of normal curves, 
+## skew, kurtosis, outliers, variability to make your conclusions. Use technical language (as per textbook) and visualizations.
+## to support your arguments. Keep your work clean with headings and comment your code.
+
+
+
+
+
+
+
+
+## CENTRAL TENDENCIES
+## Where is the middle of the data for `T2 dominance`?
+## Justify your answer using concepts of normality, and measures of central tendencies. 
+## All calculations using the manual method (but you can use built-in functions for verification)
+## Keep your work clean with headings and comment your code.
+
+
+
+
+
+
+## Concept question: can you imagine any circumstances where the mode is a better metric of central tendencies than either the median or mean?
+
+
+
+
+
+
+## VARIABILITY
+## Compute variance and standard deviation for `T2 dominance`.
+## Keep your work clean with headings and comment your code. 
+## Compute interim variables (deviation scores, sums of squares)
+
+
+
+
+
+
+
+
+## FILTERING / SUBSETTING
+## Finally, create one variable where we can see all `T2 dominance` values but only sex "1" 
+
+
+
+
+
+## Create another variable where we can see all `T2 dominance` values but only for sex "2"
+
+
+
+
+
+## Compute the mean for `T2 dominance` for each of the sexes. Do these means look different?
+
+
+
+
+
+## Speculate on possible dominance differences between the sexes. 
+## Add your remarks as hashtag comments.
+
+
+
+
