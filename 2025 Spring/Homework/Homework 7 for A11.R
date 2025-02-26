73ac317df652f6274621f54c5c3a7a2717cca7ce
Levente Orbán <llorban@users.noreply.github.com>
2025-02-25 18:49:41 -0800

    Add files via upload

---
+++ 2025 Spring/Homework/Homework 7 for A11.R
@@ -0,0 +1,7 @@
+## Homework 7 for A11
+
+## Building on the lecture content where we correlated dominance and agreeableness, 
+## take this one step further and correlate dominance with agreeableness for each sex separately.
+
+## You are permitted to do central tendency calculations using built-in functions. 
+## Covariability, standardization and correlation calculations must be done manually. Built-in functions should be used to validate your answers and troubleshoot. 
