c2f8413405d127d45ff03afdb47cc9f7237bbf78
Levente Orbán <llorban@users.noreply.github.com>
2025-02-26 17:36:24 -0800

    Create Homework 7 for A10.r

---
+++ 2025 Spring/Homework/Homework 7 for A10.r
@@ -0,0 +1,7 @@
+## Homework 7 for A10
+
+## Building on the lecture content where we correlated extraversion and dominance, 
+## take this one step further and correlate dominance with extraversion for each sex separately.
+
+## You are permitted to do central tendency calculations using built-in functions. 
+## Covariability, standardization and correlation calculations must be done manually. Built-in functions should be used to validate your answers and troubleshoot. 
