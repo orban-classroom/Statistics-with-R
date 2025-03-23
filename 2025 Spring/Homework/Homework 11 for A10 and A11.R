03c330730d9149d181202074b416cb1a416f6051
Levente Orbán <llorban@users.noreply.github.com>
2025-03-22 19:52:04 -0700

    Add files via upload

---
+++ 2025 Spring/Homework/Homework 11 for A10 and A11.R
@@ -0,0 +1,69 @@
+## Homework 11
+##############
+
+## The data set you will use is called "stressdata.csv"
+## Relevant columns: stress, 1: stressed participants, 2: not stressed participant
+## hrv_rmssd_ibi: Heart-rate variability is a measure of sympathetic nervous system functioning. Uses the small beat to beat variation of your heart beat to estimate how well your nervous system throttles between parasympathetic and sympathetic nervous system activity. Lower numbers typically mean greater stress. 
+## cortisol: Standardized cortisol measures indicating amount of cortisol expression widely considered to be the "Stress hormone"
+## Task 1: Compute the correlation between HRV (Heart-Rate Variability) and Cortisol under each stress (1) and no-stress (0) conditions. 
+## What is the relationship between heart-rate variability and cortisol? Is the relationship identical while participants are under stress or not stressed?
+## Task 2: Create a barplot of mean HRV measures for stress + no stress conditions. Add an error bar indicating SEM
+## Task 3: Create a barplot of mean cortisol measures for stress + no stress conditions. Add an error bar indicating SEM
+## Task 4: Are the HRV differences between stressed and non-stressed individuals significantly different?
+
+
+## FILTER
+## Create a filter that you can call "analysis_dataframe" or something similar that matches your
+## variable naming style, and isolate the two variables in question while also removing potential NA cells.
+
+
+
+
+
+## PRE-CHECKS
+## Generate box-plots and histograms of hrv_rmssd_ibi and cortisol variables split by stress
+## Compute skewness and kurtosis values.
+## Comment on normality (as per textbook definitions)
+
+
+
+
+## QUESTION 1
+## Compute the covariance between `hrv_rmssd_ibi` and `cortisol` under no stress, and under stress?
+## Show your work manually. Verify using built-in function and run diagnostics along the way.
+
+
+
+
+
+
+## QUESTION 2
+## Standardize the four variables and compute the correlation coefficient manually.
+## Describe your findings
+
+
+
+
+
+## Generate two scatterplots of the variables.
+## Comment on the appearance. 
+
+
+
+
+
+
+## Generate a barplot and add error bars representing SEM.
+
+
+
+
+
+## Compute an independent samples t-test to check whether the HRV (Dependent Variable) of 
+## stressed and non-stressed individuals (Independent Variable) is significantly different. 
+
+
+
+
+
+## end of homework
