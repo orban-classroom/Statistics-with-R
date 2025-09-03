3b49641baa63cfadb33f2d848f0c8e00d3457356
Levente Orbán <llorban@users.noreply.github.com>
2025-09-02 22:28:01 -0700

    Add files via upload

---
+++ 2025 Fall/Lecture Notes/Lecture 1 as delivered.R
@@ -0,0 +1,19 @@
+# Comment - whatever I write after a hashtag is not computed
+# Heading 1: Lecture 1, Introduction to R
+
+1+1 # this is addition
+1-1 # this is subtraction
+1*1
+1/1
+
+# Variable
+# <- means = 
+
+myFirstVariable <- 43
+x <- 24
+myfirstvariable <- 9
+my.first.variable <- 900 
+
+# Shortcuts: 
+# Use Ctrl + Enter as a way to activate your code
+# Use autocomplete for variable and function completions in order avoid spelling errors
