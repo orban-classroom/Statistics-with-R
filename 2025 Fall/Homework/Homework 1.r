a3528480de0bed227f52d24b367f9c18639a6257
Levente Orbán <llorban@users.noreply.github.com>
2025-09-02 22:31:52 -0700

    Add files via upload

---
+++ 2025 Fall/Homework/Homework 1.r
@@ -0,0 +1,53 @@
+## Homework 1
+
+## Instructions
+## Download this R Script and load it in your copy of R Studio
+## Enter your answers as code below each question
+## Save your work and upload to Moodle by the deadline
+## If asking question on Signal Messenger, 
+## post screenshots of errors of the entire R Studio screen (or take a photo of it)
+
+## Create a variable and assign the number 10 to it. 
+
+## for variable naming, as a standard: 
+## come up with useful, descriptive names that you can remember later
+## formatting possibilities: all in one word, capitalize the first letter of each
+## separate words by a character such a . _ -
+## cannot start a variable name with a number
+## no special characters, especially no spaces or slashes or commas (and many other things)
+## do not make a variable name the same as a function word
+## e.g., mean() is a function. if you make your variable name mean, you set the path for confusion land
+# mean <- 10
+# mean
+# mean(10,11,12)
+
+## Create a second variable and assign the number 52 (which is the meaning of life, if you watched Hitchiker's Guide to the Galaxy)
+
+## Add the two numbers together using their variable names
+
+## Subtract the second variable from the first variable.
+
+## Functions are built-in commands on R that do some internal computation
+## For example, the function rm() deletes a variable from the global environment
+## Delete the second variable from the global environment
+
+## When you don't understand how a function works, you can type in
+## the function's name with a question mark before it, like so ?rm()
+## Type ?c() and read the help document (bottom right quadrant)
+
+## Create a vector using c() that contains last week's measles infections by province.
+## Use this link: https://www.canada.ca/en/public-health/services/publications/diseases-conditions/measles-rubella-surveillance/2024/week-16.html
+
+## Check the class of the data set. 
+
+## Convert the data to be an integer data type
+
+## In R, you can create different data types. Until now you have been dealing with one type
+## Using the class() function, find out what kind of data have you been dealing with
+## Figure it for the first variable (the second one has been deleted), and for the vector your created.
+
+
+
+
+## end of homework
+##################
