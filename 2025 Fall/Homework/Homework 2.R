5c7a299fe4da252853e010dbf2ca69f07b94a1e4
Levente Orbán <llorban@users.noreply.github.com>
2025-09-09 22:23:27 -0700

    Add files via upload

---
+++ 2025 Fall/Homework/Homework 2.R
@@ -0,0 +1,52 @@
+## R Homework 2
+
+## Follow these stylistic conventions
+## 1. Refine your own system of variable naming, and stick to it. I will be looking for consistency over the semester. You can still play arund now.
+## 2. Use a single hashtag # for your comments. I will use ## for my comments.
+## Use spaces carefully to organize your code. Make it readable. Create headings and comments in your code.  
+
+## Look in the 2025 class folder for the latest version of the 2300 class data, "202510_2300_classdata.csv"
+## Import it here and assign it to a data frame variable.
+
+
+## Use the colnames() to figure out the name of every column in the data set
+
+  
+## Use the nrow() function to figure out how many records exist in the data set
+
+
+## Generate a quick peak into the data using head(). 
+
+
+## Add an argument in the head() function to change the default 6 rows to 3 rows
+
+
+## Can you find a function to output the last 6 rows instead of the first 6 rows?
+
+
+## Generate a histogram of the "dependents" variable
+## Learn to use arguments in the hist() function in order to generate a graph title, and change the y-axis and x-axis label
+## Change the colours of the histogram to make it look pretty. Brownie points for good looking graphs
+
+
+
+## Sort the values no.courses.taken variables. What is the fewest and greatest number of courses students are taking? 
+## Output the result below
+
+
+## Consider the following conceptual question.
+## Ackerman and Goldsmith (2011) compared learning performance for students who 
+## studied material printed on paper versus students who studied the same material 
+## presented on a computer screen. All students were then given a test on the 
+## material and the researchers recorded the number of correct answers. 
+
+## Identify the dependent variable for this study
+
+
+## In R, how would you encode the dependent variable? Numeric, string, or integer?
+
+
+## What scale of measurement (nominal, ordinal, interval, ratio) is used to measure the dependent variable?
+
+
+## end of homework
