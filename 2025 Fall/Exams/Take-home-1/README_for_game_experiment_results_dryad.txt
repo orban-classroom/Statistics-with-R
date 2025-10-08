e6cfa0f31f09a8e260025279ac423793748d4cab
Levente Orbán <llorban@users.noreply.github.com>
2025-10-07 18:52:45 -0700

    Take-home exam 1

---
+++ 2025 Fall/Exams/Take-home-1/README_for_game_experiment_results_dryad.txt
@@ -0,0 +1,29 @@
+Variable illumination:
+
+
+ID: The unique individual ID of each participant
+
+CS: The size of communities where each participant lives. "big" as big community,"small" as small community.
+
+Choice: The institution choice of each participant at the beginning of the whole experiment process. Religious instituion choice code as"1",nonreligious institution choice as "0".
+		
+Dice game  kept: The amount of endowment kepy by participants in each round of dice allocation game.
+	
+Free donation amount: The amount of endowment donated by participants in eaach round of public doantion game.
+	
+PC1:  PCA scores for a combined measure of religious practice and behavior, standardized by using z-score normalization.
+	
+PC2:  PCA scores for the degree of personal belief in divine reward for good acts or divine punishment for bad behavior, standardized by using z-score normalization.
+	
+PC3: PCA scores for the belief in numerous other superstitions, standardized by using z-score normalization.
+
+
+
+
+
+
+
+
+
+Any problems please contact geerhao@gmail.com
+
---
+++ 2025 Fall/Exams/Take-home-1/takehome1-a10.R
@@ -0,0 +1,96 @@
+## TAKE-HOME EXAM 1
+## PSYC 2300 - APPLIED STATISTICS 1
+## INTENDED DURATION: 2 hours (not monitored)
+
+## EXAM REGULATIONS: 
+## 1. Do not discuss the exam with anyone.
+## 2. You can message Dr. Orban with any questions, including feedback on homework performance to assist with answers on this exam.
+## 3. You may use resources such as the textbook, lecture slides, lecture R notes and homework to complete this exam. 
+## 4. Do not talk to classmates, tutors or other humans about this exam.
+## 5. You can use your textbook, lecture notes, LLM's, AI and online sources to complete this exam.
+## 6. IMPORTANT: You grade on this exam is based on ORALLY DEFENDED answers. 
+##    This means that if you don't understand your own code at all, it is possible to receive a 0 on a perfect written exam.
+## This is an exam. Rule violations will be investigated by the dean's office for academic integrity violation. 
+
+## About the Data Set: game_experiment_results_dryad.csv
+## Article Link: https://royalsocietypublishing.org/doi/10.1098/rsos.190991
+## The data set describes donation dollar amounts in a game where subjects were
+## recruited from "small" or "big" communities.
+## File name on GitHub (inside the "Exams" folder): 
+
+## Variables you will use are: "CS", "FreeDonationAmount"
+
+
+## NORMALITY
+## Assess `FreeDonationAmount` for normality. Draw on your of knowledge of normal curves, 
+## skew, kurtosis, outliers, variability to make your conclusions. Use technical language (as per textbook) and visualizations.
+## to support your arguments. Keep your work clean with headings and comment your code.
+
+
+
+
+
+## CENTRAL TENDENCIES
+## Where is the middle of the data for `FreeDonationAmount`?
+## Justify your answer using concepts of normality, and measures of central tendencies. 
+## All calculations using the manual method (but you can use built-in functions for verification)
+## Keep your work clean with headings and comment your code.
+
+
+
+
+## Concept question: can you imagine any circumstances in which a median is a better metric of central tendencies than the mean?
+
+
+
+## VARIABILITY
+## Compute variance and standard deviation for `FreeDonationAmount`.
+## Keep your work clean with headings and comment your code. 
+## Compute interim variables (deviation scores, sums of squares)
+
+
+
+
+
+## FILTERING / SUBSETTING
+## Create one filter (see Homework 4 for similar filtetring) where 
+## you can see all `FreeDonationAmount` values but only `CS` (community size) that is "small" 
+
+
+
+
+## Create another filter where your can see all `FreeDonationAmount` values 
+## but only `CS` that is "big"
+
+
+
+
+
+## Compute the mean for `FreeDonationAmount` for each of the community sizes. 
+
+## small
+
+
+## big
+
+
+## Do these means look different? Comment with a hashtag below
+
+
+
+
+## Now compute the standard deviation for each community size
+
+## small
+
+
+## big
+
+
+## Comment on the size of the standard deviation in relation to the mean
+## Add your remarks as hashtag comments.
+
+
+
+
+## END OF EXAM.
