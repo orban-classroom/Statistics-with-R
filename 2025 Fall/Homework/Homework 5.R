2f3ed90af0151fa79fec1e2a1357ba99138be8ed
Levente Orbán <llorban@users.noreply.github.com>
2025-10-27 08:04:58 -0700

    Add files via upload

---
+++ 2025 Fall/Homework/Homework 5.R
@@ -0,0 +1,55 @@
+## Homework 5
+##############
+
+## You will use a new data set for this homework, dealing with disgust sensitivity
+## The data package and description is in the Data/disgustsensitivity folder.
+## Read about the data set by looking at the "10-91-1-PB.pdf" file.
+
+## Load the disgust sensitivity data from the GitHub folder:
+## Load the data file: Berger_and_Anaki_disgust_scale_2014_1_comma_delimited_cleaned.csv
+
+## You will deal with two variables: `Age`, `Mean_general_ds`
+## Note: Mean_general_ds refers to disgust sensitivity
+
+## LOADING DATA
+## Bring the data here from GitHub directly (not from your local computer)
+
+url <- "<<replace this text>>"
+ds_data <- read.csv(url)
+
+## FILTER
+## Create a filter to isolate the two variables in question while also removing potential NA cells.
+
+
+
+## PRE-CHECKS
+## Generate box-plots and histograms of each of the variables 
+## Compute skewness and kurtosis values.
+## Comment on normality (as per textbook definitions)
+
+
+
+
+## TASK 1
+## Standardize the two variables.
+## Describe your findings
+
+
+
+
+## TASK 2
+## Compute the covariance between `Age` and `Mean_general_ds`?
+## Show your work manually. Verify using built-in function and run diagnostics along the way.
+
+
+
+
+
+## TASK 3 Generate a scatterplot of the variables.
+## Comment on the appearance. 
+
+
+
+
+
+## end of homework
