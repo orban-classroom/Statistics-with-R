51f55f4344be5041d94bd759d6da8fb50482b6a2
Levente Orbán <llorban@users.noreply.github.com>
2025-10-30 15:49:29 +1200

    Add files via upload

---
+++ 2025 Fall/Homework/Homework 6.R
@@ -0,0 +1,34 @@
+## Homework 6
+
+
+## Load the Updated PSYC 2300-30 Class Data file
+
+
+## You will work with "# of courses taken" and "hrs of study weekly" variables
+
+
+## Hypothesis: Those who take more courses will spend more time studying
+
+
+## Measure the normality of each variable (comment on skewness, kurtosis). 
+## Include relevant computations, and visualisations to make a determination. 
+
+
+
+## Compute the variance of each variable separately
+
+
+
+## Standardize each variable
+
+
+
+## Compute the correlation coefficient (i.e., standardized covariance) of each variable
+
+
+
+## What is your research conclusion?
+
+
+
+## end of homework
