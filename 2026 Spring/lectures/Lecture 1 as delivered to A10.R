bf0082e2130fa314517014c3cd3c9fd614cc62d6
Levente Orbán <llorban@users.noreply.github.com>
2026-01-07 18:15:53 -0800

    Add files via upload

---
+++ 2026 Spring/lectures/Lecture 1 as delivered to A10.R
@@ -0,0 +1,20 @@
+## Lecture 1, as delivered to A10
+## January 7, 2026
+
+1+1
+# To Active the Code: 
+# On Windows: Ctrl + Enter
+# On Mac: command + return
+
+2+3 
+4+4
+5-5
+5-1
+
+1*1
+2*2
+3*3
+
+3/3
+2/1
+10/1
