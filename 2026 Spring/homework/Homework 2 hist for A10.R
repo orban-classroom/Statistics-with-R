122cfd7a99de1884a8532a4559d947fe65b3d727
Levente Orbán <llorban@users.noreply.github.com>
2026-01-14 22:19:18 -0800

    Add files via upload

---
+++ 2026 Spring/homework/Homework 2 hist for A10.R
@@ -0,0 +1,48 @@
+## R Homework 2
+
+## Follow these stylistic conventions
+## 1. Refine your own system of variable naming, and stick to it. I will be looking for consistency over the semester. You can still play arund now.
+## 2. Use a single hashtag # for your comments. I will use ## for my comments.
+## Use spaces carefully to organize your code. Make it readable. Create headings and comments in your code.  
+
+## Here is a variable of collected data from several past PSYC 2300 classes
+hours.of.screentime.per.week <- c(20, 14, 30, 35, 25, 30, 12, 14, 98, 55, 25, 30, 14, 21, 20, 10.5, 20, 28, 30, 14, 20, 14, 12, 25, 30, 42, 38, 30, 35, 25, 40, 50, 50, 55, 35, 42, 40, 20, 40, 40, 28, 28, 48, 20, 28, 30, 32, 89, 28, 30, 40, 50, 42, 25, 84, 28, 35, 45, 6, 30, 40, 42, 30, 15, 30, 21, 25, 42, 10, 28, 70, 21, 70, 20, 24, 77, 30, 20, 35, 70, 30, 25, 17, 6, 40, 20, 56, 21, 37, 28, 11, 25, 18, 30, 60, 28, 28, 13, 10, 35, 24, 29, 15, 20, 15, 25, 28, 50, 7, 40, 40, 30, 25, 22, 6, 56, 13, 28, 6, 4, 21, 50, 21, 50, 27, 5, 20, 19, 42, 50, 21, 30, 42, 126, 28, 10, 14, 30, 48, 20, 30, 14, 85, 40, 55, 21, 56, 50, 6, 35, 14, 42, 28, 25, 35, 105, 20, 42, 35, 45.5, 56, 50, 40, 35, 42, 56, 35, 42, 15, 35, 20, 30, 30, 15, 10, 20, 20, 90, 40, 35, 70, 50, 47, 30, 25, 70, 45, 21, 35, 15, 40, 20, 18, 15, 80, 52, 20, 20, 7.5, 21, 12, 60, 30, 28, 6)
+
+## Use the length() to figure out how many data points you have in this data set.
+
+
+## Generate a quick peak into the data using head(). 
+
+
+## Add an argument in the head() function to change the default 6 rows to 3 rows
+
+
+## Can you find a function to output the last 6 rows instead of the first 6 rows?
+
+
+## Generate a histogram of the data
+## Learn to use arguments in the hist() function in order to generate a graph title, and change the y-axis and x-axis label
+## Change the colours of the histogram to make it look pretty. Brownie points for good looking graphs
+
+
+
+## Sort the values in order going from smallest to greatest. What is the smallest and largest value in the data set? 
+
+
+## Consider the following conceptual question.
+## Ackerman and Goldsmith (2011) compared learning performance for students who 
+## studied material printed on paper versus students who studied the same material 
+## presented on a computer screen. All students were then given a test on the 
+## material and the researchers recorded the number of correct answers. 
+## Add your answers as # hashtag comments
+
+## Identify the dependent variable for this study
+
+
+## In R, how would you encode the dependent variable? Numeric, string, or integer?
+
+
+## What scale of measurement (nominal, ordinal, interval, ratio) is used to measure the dependent variable? How would this be mapped in R?
+
+
+## end of homework
