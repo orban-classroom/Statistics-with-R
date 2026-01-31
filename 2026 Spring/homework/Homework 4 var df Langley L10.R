77b16ad95ba4b2efce4ab15b8507dacdbcbc4314
Levente Orbán <llorban@users.noreply.github.com>
2026-01-31 08:57:01 -0800

    Add files via upload

---
+++ 2026 Spring/homework/Homework 4 var df Langley L10.R
@@ -0,0 +1,52 @@
+## Homework 4
+############
+
+## Load the PSYC2300 class survey data here.
+
+
+## Use the exercise variable for this homework. Develop your filter below.
+
+
+## Create two types of filters: 
+## Pick out all the winter semester classes (while looking at exercise only). 
+## Develop your filter below.
+
+
+## Pick out all the summer semesters (while looking at exercise only). 
+
+
+## For each of filters, complete these steps:
+## Generate a histogram for each (exercise winter, exercise summer). 
+## Add hashtag comments on the shape of the distribution
+## Use the method described in this Stack Overflow article to produce two histograms 
+## blended together to show the number of dependents in each of the classes: 
+## https://stackoverflow.com/questions/3541713/how-can-i-plot-two-histograms-together-in-r
+## Look at the answer posted by the user named "Dirk is no longer here"
+
+
+
+## Compute the mean for summer exercise, winter exercise manually. Verify your work using the built-in function
+
+
+
+## Compute the sums of squares for summer exercise, winter exercise manually. 
+
+
+## Make diagnostic remarks as in class to gauge whether your calculations are correct
+
+
+
+## Compute the variance manually. Verify your calculation using the built-in function
+
+
+
+## Compute the standard deviation manually. Verify your calculation using the built-in function
+
+
+
+## Do the summer exercise and winter exercise numbers look different?
+
+
+
+
+
