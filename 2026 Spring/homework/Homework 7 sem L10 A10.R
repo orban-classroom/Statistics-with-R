d778a0f40ef7cb538fff381c79cc7346c51b139a
Levente Orbán <llorban@users.noreply.github.com>
2026-03-14 20:05:06 -0700

    Add files via upload

---
+++ 2026 Spring/homework/Homework 7 sem L10 A10.R
@@ -0,0 +1,24 @@
+## Homework 7 for L10 and A10
+
+## Central Limit Theorem and Standard Error of the Mean
+
+
+## Load the Rawlings_et_al_chimp_LT_personality_data.csv data
+
+
+## Research Question: Is Sex 1 or Sex 2 the more dominant personality among chimps in T1
+
+
+## Sanitization and Filter
+
+
+## Normality Check using methods discussed in class
+
+
+## Bar plot with Standard Error of the Mean as error bars
+
+
+## Interpret the results
+
+
+## Repeat for the following question: Is Sex 1 or 2 showing more openness in T1?
