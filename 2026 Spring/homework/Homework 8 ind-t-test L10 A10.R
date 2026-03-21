1164c7014de156bdf21444284049e53b31b13960
Levente Orbán <llorban@users.noreply.github.com>
2026-03-20 21:52:55 -0700

    Add files via upload

---
+++ 2026 Spring/homework/Homework 8 ind-t-test L10 A10.R
@@ -0,0 +1,36 @@
+## Homework 8 for L10 and A10
+
+## Load the stressdata.csv file from the Github/data directory
+
+## RQ: Do subjects coded as "1" stressed have higher cortisol levels than subjects coded as "0"?
+
+## State the hypothesis: 
+
+## State the statistical hypothesis:
+
+## Make a decision on the type of t-test you will conduct: 
+
+## Filter + Data Sanitation
+#### Normality
+
+#### Histograms
+
+#### Boxplots
+
+#### Homogeneity
+
+## Intermediate calculations
+
+## Sample size + degrees of freedom
+
+#### Means
+
+#### Pooled variance
+
+## t-test
+
+## Bar plot with Standard Error of the Mean as error bars
+
+## Interpret the results
+
+
